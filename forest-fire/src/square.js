f261ac7fe2fc40e60627669d688e1ee310610f96
Michał Pałys <michal.palys.konto@gmail.com>
2021-04-13 22:03:32 +0200

    added programmable squareWidth and squareheight

--- forest-fire/src/square.js
+++ forest-fire/src/square.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 const n = 10; // ilość wierszy
 const m = 10; // ilość kolumn
+const squareHeight = 100; // wysokość kwadratu w px
+const squareWidth = 100; // szerokość kwadratu w px
+
 
 const Square = () => {
     const [color, setColor] = React.useState('lightgreen');
     const squareStyle = {
         outline: 'none',
-        height: '100px',
-        width: '100px',
+        height: `${squareHeight}px`,
+        width: `${squareWidth}px`,
         float: 'left', // kolejne kwadraty nie rozszerzają kontenera, ustawiają się poniżej
         borderRadius: '10px',
         background: `${color}`, // handleClick zmienia stan z useState i tym samnym kolor tutaj
@@ -32,20 +35,27 @@ const App = () => {
             <h1>Forest fire</h1>
             <div
                 style={{
-                    display: 'table',
-                    height: '1000px', // 10 wierszy
-                    width: '1000px', // 10 kolumn
-                    border: '1px solid',
-                    background: 'green',
-                    boxShadow: '10px 10px 10px lightgray',
+                    display: 'flex',
+                    justifyContent: 'center',
                 }}>
-                {Array(noSquares)
-                    .fill()
-                    .map((x, i) => (
-                        <Square key={i} />
-                    ))}
+                <div
+                    style={{
+                        display: 'table',
+                        height: `${n * squareHeight}px`, // n wierszy
+                        width: `${m * squareWidth}px`, // m kolumn
+                        border: '1px solid',
+                        background: 'green',
+                        boxShadow: '10px 10px 10px lightgray',
+                    }}>
+                    {Array(noSquares)
+                        .fill()
+                        .map((x, i) => (
+                            <Square key={i} />
+                        ))}
+                </div>
             </div>
         </div>
+
     );
 };
 
