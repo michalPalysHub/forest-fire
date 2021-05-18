6c3225c4de2b7f2f1e9ddc361ac41cfc77852ce6
kz3ko <xiixikx@gmail.com>
2021-05-18 19:28:53 +0200

    Colors of squares borders defined.

--- src/components/square.jsx
+++ src/components/square.jsx
@@ -8,6 +8,20 @@ const forestTypeToSquareColorDict = {
     3: 'darkgreen',     // iglasty
 };
 
+const forestStateToSquareBorderColorDict = {
+    0: null, // Domyślnie brak koloru krawędzi.
+    1: '#0CF228', // Zagrożenie niskie.
+    2: '#0E921E', // Zagrożenie umiarkowane.
+    3: '#8A920E', // Zagrożenie wysokie.
+    4: '#F0B708', // Zagrożenie bardzo wysokie.
+    5: '#F09108', // Zagrożenie ekstremalne.
+    6: '#FE0101', // Pożar łatwy do opanowania.
+    7: '#C50000', // Pożar trudny, ale możliwy do opanowania.
+    8: '#830000', // Pożar niemożliwy do opanowania.
+    9: '#000000', // Sektor spalony.
+}
+
+// Słownik umożliwiający reprezentację stanu zagrożenia pożarem lub jego zaawansowania dla danego sektora.
 const Square = (props) => {
     // Zmienne stanu reprezentujące współrzędne służące do identyfikacji danego kwadratu
     const [i, setRowIndex] = useState(props.i);
@@ -16,6 +30,10 @@ const Square = (props) => {
     // Zmienna określająca typ lasu dla danego kwadratu
     const [forestType, setForestType] = useState(0)
 
+    // Zmienna przechowująca stan danego sektora.
+    const forestState = 0;
+    console.log(forestState)
+
     // Informacje na temat stylu w CSS
     const squareStyle = {
         outline: 'none',
@@ -23,7 +41,8 @@ const Square = (props) => {
         width: `${props.size}px`,
         float: 'left', // kolejne kwadraty nie rozszerzają kontenera, ustawiają się poniżej
         borderRadius: '1px',
-        borderWidth: '1px',
+        borderWidth: '2px',
+        borderColor: `${forestStateToSquareBorderColorDict[forestState]}`,
         opacity: 0.6,
         background: `${forestTypeToSquareColorDict[forestType]}`,
     };
